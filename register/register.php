76e93a2265315b1788658f4e65e0f58d117bd71f
phpuser <13802883435@139.com>
2016-08-08 07:28:48 -0400

    register.php添加异常判断

--- register/register.php
+++ register/register.php
@@ -11,12 +11,21 @@ include '../mysql_lib/mysql_basic_lib.php';
 		$psw_confirm=$_POST["confirm"];
 		$id_code=$_POST["idcode"];
 		
-		//echo "test";
+		//异常情况判断，避免错误输入
+		if(!$user)
+		{
+			echo "用户名不能为空！<br/>";
+		}
+		else if((!$psw)||(!$psw_confirm))
+		{
+			echo "密码不能为空！<br/>";
+		}
 		
 		$con=mysqlconnect($host_name,$user_name,$db_password);		//连接数据库
 		if($con)
 			echo "数据库连接成功啦！<br/>";
-		//createdatabase($con,$db_name);							//创建数据库
+		//createdatabase($con,$db_name);				//创建数据库
+		mysql_close($con);						//关闭数据库
 	}
 	//echo "here";
 	//echo $db_password;
